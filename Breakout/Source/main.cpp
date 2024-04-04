2aec8bc1ee4890182127877222ee7ce2751b80bf
Tycho <t.charko@outlook.com>
2024-04-04 23:12:38 +0200

    SDL2 🛠
    - Added SDL2 (2.30.2) to the project
    - Simple window & screen render

--- Breakout/Source/main.cpp
+++ Breakout/Source/main.cpp
@@ -1,4 +1,22 @@
+#include <SDL.h>
+#undef main
+
 int main()
 {
+	//Setup Rendering
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Breakout", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720,SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+
+	//Render simple color
+
+	SDL_SetRenderDrawColor(renderer, 27, 146, 214, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(3000);
+
 	return 0;
 }
