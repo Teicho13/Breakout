1563bd0b0a510710f2728b9059858c59549ffa78
Tycho <t.charko@outlook.com>
2024-04-10 18:08:06 +0200

    Prevent Moving outside border 🔳

--- Breakout/Source/Entities/Vaus.cpp
+++ Breakout/Source/Entities/Vaus.cpp
@@ -7,6 +7,14 @@ Vaus::Vaus(const char* filePath, SDL_Renderer* renderer, float width, float heig
 void Vaus::Move(int direction, float dt)
 {
 	SDL_FRect tmpRec = GetPosition();
+
+	//TODO: make this not a magic number / get window width
+	if (tmpRec.x + tmpRec.w >= 1280.f && direction == 1)
+		direction = 0;
+
+	if (tmpRec.x <= 0.f && direction == -1)
+		direction = 0;
+
 	tmpRec.x += m_Speed * direction * dt;
 	SetPosition(tmpRec);
 }
